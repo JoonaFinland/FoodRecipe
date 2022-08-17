b23d89af09c26da0492fed46a8ef6e46c1946b83
Joona <43171241+JoonaFinland@users.noreply.github.com>
2022-08-17 18:55:16 +0300

    Get Django secret key from environment variables

--- Backend/FoodRecipe/settings.py
+++ Backend/FoodRecipe/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^xq*7y1hs*)94i^+y!re79&(6x+17nkr+qfrmupwt+!77g^8kq'
+SECRET_KEY = os.getenv('DJANGO_SECRET', 'None')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
